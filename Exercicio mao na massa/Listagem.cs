a58ff5b2441efe1783f7a8df4b37e04d0886093c
Lucas <luckas1695@gmail.com>
2018-11-19 16:09:11 -0200

    listagem de arqivos implementado.

---
+++ Exercicio mao na massa/Listagem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_mao_na_massa
+{
+    public class Listagem
+    {
+
+        public void listarArquivos()
+        {
+            Console.Clear();
+            Console.Write("\n [Utilize o nome completo. EX.: c:\\temp\\]");
+            Console.Write("\n Digite o caminho: ");
+            string nome = Console.ReadLine();
+
+            if (Directory.Exists(nome))
+            {
+                string[] informacoes = Directory.GetFiles(nome);
+
+                foreach (var info in informacoes)
+                {
+                    Console.WriteLine(info);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Diretório especificado não existe.");
+            }
+        }
+    }
+}
---
+++ Exercicio mao na massa/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_mao_na_massa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Listagem lista = new Listagem();
+
+            lista.listarArquivos();
+
+            Console.ReadKey();
+        }
+    }
+
+}
